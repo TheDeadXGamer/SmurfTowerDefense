d28b14fa66c4258e10a3220bfac0e61ec1bfe285
timpan21 <timnh1722@hotmail.com>
2024-11-18 13:06:17 +0100

    MVC and changes in Player class to follow SRP

--- Model/Player/PlayerInterface.java
+++ Model/Player/PlayerInterface.java
@@ -1,9 +1,7 @@
 package Player;
-
 import java.util.List;
-import Game.*;
-
 import Tower.*;
+
 interface PlayerInterface{
     // Arvid: should be split into smaller interfaces (interface segregation principle), see similar methods in EnemyInterface
 
@@ -14,26 +12,11 @@ interface PlayerInterface{
     int getHealth();
     void setHealth(int health);
 
-    int getRoundNumber();
-    void setRoundNumber(int roundNumber);
-    
-    // Add or subtract money
-    void modifyMoney(int amount);
-
 
     // Manage towers owned py player
     List<Tower> getTowers();
     void addTower(Tower tower);
     void removeTower(Tower tower);
-
-    // Check or update the players satus ingame, won? lost? round
-    GameStatus getStatus();
-    void setStatus(GameStatus status);
-
-    Difficulty getDifficulty();
-
-    // Applies modifiers depending on the selected difficulty 
-    void applyDifficultyModifiers();
 
     // Checks if the player is still alive, i.e checking if health is above 0
     boolean isAlive();
