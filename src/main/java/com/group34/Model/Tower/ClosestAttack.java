9ee4df8a0119a437b78e5cb724f1d9a8f77d9c08
Jonas Lutschke <lutschke@chalmers.se>
2024-11-22 12:31:00 +0100

    Removed idea and vscode folders

--- src/main/java/com/group34/Model/Tower/ClosestAttack.java
+++ src/main/java/com/group34/Model/Tower/ClosestAttack.java
@@ -7,14 +7,14 @@ import java.util.stream.Stream;
 
 import com.group34.Model.Enemy.BaseEnemy;
 
-public interface closestAttack extends Attack {
+public interface ClosestAttack extends Attack {
     default public void attack(Stream<BaseEnemy> enemies) {
 
-        BaseEnemy closest = enemies
+        /*BaseEnemy closest = enemies
             .min(Comparator.comparing(target -> this.getPosition().distance(target.getPosition()))).get();
         int currHealth = closest.getHealth();
         closest.setHealth(currHealth - this.damage());
-
+        */
     };
     
 }
