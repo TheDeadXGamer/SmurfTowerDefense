8b23958a2a4427c96f5b8bbeb3af1d6c6de63602
Jonas Lutschke <lutschke@chalmers.se>
2024-11-22 12:42:33 +0100

    Commented out future code

--- src/main/java/com/group34/Model/Tower/AreaAttack.java
+++ src/main/java/com/group34/Model/Tower/AreaAttack.java
@@ -8,10 +8,12 @@ import com.group34.Model.Enemy.BaseEnemy;
 
 public interface AreaAttack extends Attack {
     default public void attack(Stream<BaseEnemy> enemies) {
+        /*
         enemies.forEach(target -> {
             int currHealth = target.getHealth();
             target.setHealth(currHealth - this.damage()); 
         });
+        */
 
     };
     
