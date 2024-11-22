b4401e20dd171818c1ffe8e64a8a4a064811a84e
Arvid Hammarlund <101809834+ArvidHammarlund@users.noreply.github.com>
2024-11-22 12:33:30 +0100

    Update ClosestAttack.java

--- src/main/java/com/group34/Model/Tower/ClosestAttack.java
+++ src/main/java/com/group34/Model/Tower/ClosestAttack.java
@@ -7,13 +7,13 @@ import java.util.stream.Stream;
 
 import com.group34.Model.Enemy.BaseEnemy;
 
-public interface closestAttack extends Attack {
+public interface ClosestAttack extends Attack {
     default public void attack(Stream<BaseEnemy> enemies) {
 
         BaseEnemy closest = enemies
-            .min(Comparator.comparing(target -> this.getPosition().distance(target.getPosition()))).get();
-        int currHealth = closest.getHealth();
-        closest.setHealth(currHealth - this.damage());
+            .filter(enemy -> this.getPosition().distance(enemy.getPosition()).norm() < this.range())
+            .min(Integer::compare)
+            .map(enemy -> enemy.damage(this.damage));
 
     };
     
