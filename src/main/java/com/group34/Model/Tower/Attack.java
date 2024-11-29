366a93bb15c41466f3bdb9baead2636b8a41b6ff
Arvid Hammarlund <101809834+ArvidHammarlund@users.noreply.github.com>
2024-11-29 14:14:31 +0100

    Comments for Tower

--- src/main/java/com/group34/Model/Tower/Attack.java
+++ src/main/java/com/group34/Model/Tower/Attack.java
@@ -1,13 +1,6 @@
 package com.group34.Model.Tower;
 
-import java.util.stream.Stream;
-
-import com.group34.Model.Enemy.Enemy;
-
 public interface Attack extends Tower {
    public int getAttackSpeed();
    public int getDamage();
-
-
-
 }
