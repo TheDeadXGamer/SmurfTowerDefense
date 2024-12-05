b8397196c21fa4fb9158b082911fe8295c58f935
Arvid Hammarlund <101809834+ArvidHammarlund@users.noreply.github.com>
2024-12-05 20:09:34 +0100

    main loop builder

--- src/main/java/com/group34/Main.java
+++ src/main/java/com/group34/Main.java
@@ -1,31 +1,77 @@
 package com.group34;
 
 import java.awt.Dimension;
+import java.util.List;
 
 import javax.swing.JFrame;
 
 import com.group34.Model.Board.Board;
+import com.group34.Model.Cash.CashVault;
+import com.group34.Model.Game.Game;
+import com.group34.Model.Game.Player;
+import com.group34.Model.Round.Round;
 import com.group34.View.BoardView;
 import com.group34.View.LightningSmurfView;
 
 
 
+class TowerDefenceBuilder {
+    Board board;
+    List<Round> rounds;
+    CashVault cashVault;
+    Game game;
+    Player player;
+
+    public TowerDefenceBuilder setBoard(Board board) {
+        this.board = board;
+        return this;
+    }
+
+    public TowerDefenceBuilder setPlayer(Player player) {
+        this.player = player;
+        return this;
+    }
+
+    public TowerDefenceBuilder setRounds(List<Round> rounds) {
+        this.rounds = rounds;
+        return this;
+    }
+
+    public TowerDefenceBuilder setCashVault(CashVault cashVault) {
+        this.cashVault = cashVault;
+        return this;
+    }
+
+    public TowerDefenceBuilder setGame(Game game) {
+        this.game = game;
+        return this;
+    }
+
+    public TowerDefence build() {
+        return new TowerDefence(this);
+    }
+}
+
 class TowerDefence extends JFrame implements Runnable {
     static final int FPS = 60;
+    private CashVault cashVault;
+    private Game game;
+    private Board board;
+    private List<Round> rounds;
+    private Player player;
 
-    //Game game;
-    //Board board;
-    //Player player;
+    public TowerDefence(TowerDefenceBuilder builder) {
 
-    public TowerDefence() {
-        //this.board = board;
-        //this.player = player;
+        this.board = builder.board;
+        this.cashVault = builder.cashVault;
+        this.game = builder.game;
+        this.rounds = builder.rounds;
+        this.player = builder.player;
 
 
         init();
 
     }
-
 
     void init() {
         setTitle("Tower Defence");
@@ -33,9 +79,9 @@ class TowerDefence extends JFrame implements Runnable {
         setResizable(false);
         setLocationRelativeTo(null);
         
-        Board board = new Board(new Dimension(815, 635));
-        BoardView boardView = new BoardView(board);
-        boardView.add(new LightningSmurfView(board));
+        // Board board = new Board(new Dimension(815, 635));
+        BoardView boardView = new BoardView(this.board);
+        boardView.add(new LightningSmurfView(this.board));
         add(boardView);
         
         pack();
@@ -44,8 +90,8 @@ class TowerDefence extends JFrame implements Runnable {
 
     @Override
     public void run() {
-        while (true) {
-            System.out.println("Game loop");
+        while (player.isAlive()) {
+            game.update();
             repaint();
         }
     }
@@ -57,9 +103,17 @@ class TowerDefence extends JFrame implements Runnable {
 
 public class Main {
     public static void main (String[] args){
+    
+        TowerDefence towerDefence = new TowerDefenceBuilder()
+            .setBoard(new Board(new Dimension(815, 635)))
+            .setRounds(null)
+            .setPlayer(new Player(30))
+            .setCashVault(new CashVault(300))
+            .setGame(new Game())
+            .build();
 
-       Thread thread = new Thread(new TowerDefence());
-       thread.start();
+        Thread thread = new Thread(towerDefence);
+        thread.start();
 
     }
 }
