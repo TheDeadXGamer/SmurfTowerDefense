a361a6f3aa7a012982be528110fac5bf1087342c
Erik Zetterlund <7anerik@gmail.com>
2024-12-17 12:39:32 +0100

    Javadoc for towers and projectiles

--- src/main/java/com/group34/Model/Tower/TowerFactory.java
+++ src/main/java/com/group34/Model/Tower/TowerFactory.java
@@ -4,7 +4,11 @@ import java.awt.geom.Point2D;
 
 public abstract class TowerFactory {
 
-    
+    /**
+     * Creates a tower at the given position.
+     * @param position the position of the created tower.
+     * @return the created tower.
+     */
     abstract public Tower createTower(Point2D position);
     
     
