276fb5de80beb2ba5c930f78ea078c96fee27629
Erik Zetterlund <7anerik@gmail.com>
2024-12-19 23:39:23 +0100

    fixed update

--- src/main/java/com/group34/Controller/TowerUpgrade.java
+++ src/main/java/com/group34/Controller/TowerUpgrade.java
@@ -1,6 +1,7 @@
 package com.group34.Controller;
 
 import com.group34.Model.Shop.CashVault;
+import com.group34.Model.Shop.OverDraftError;
 import com.group34.Model.Tower.Tower;
 import com.group34.View.BoardView;
 
@@ -9,7 +10,17 @@ public class TowerUpgrade {
     private BoardView boardView;
 
     public void upgradeTower(Tower tower)  {
-
+        try {
+            Tower upgradedTower = tower.upgrade();
+            if (upgradedTower != null) {
+                cashVault.withdraw(upgradedTower.getCost());
+            } else {
+                boardView.showTemporaryMessage("Cant upgrade this tower further!");
+            }
+            boardView.rightPanel.displayUpgradePanel(tower);
+        } catch (OverDraftError e) {
+            boardView.showTemporaryMessage(e.getMessage());
+        }
     }
 
     public void setCashVault(CashVault cashVault) {
@@ -20,3 +31,4 @@ public class TowerUpgrade {
         this.boardView = boardView;
     }
 }
+
